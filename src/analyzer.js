ed7010eb5bcbb48c0a2c8079284c93f45a87e88e
Elicrisostomo25 <152878721+Elicrisostomo25@users.noreply.github.com>
2024-02-01 11:43:33 -0300

    final

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,115 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordCount = text.trim().split(/\s+/g);
+    const contarPalabras = wordCount.filter(function (elemento){
+      return isNaN(elemento); // Devuelve true si el elemento no es un número
+    });      
+    console.log(contarPalabras); 
+    //let soloPalabras = wordCount.replace((?<=^|,)\d+\.\d{2}(?=,|$), "");//aplicar el metodo replace para elimiar los numeros. o metodo filter para que no devuelva numeros y letras
+    return contarPalabras.length;
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
 
-export default analyzer;
+  getCharacterCount: (text) => {
+    const characterCount = text.split("").length;
+    return characterCount;
+  },
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const characterCountExcludingSpaces = text.replace(/\W/g, "");
+    const caracteresEspeciales = characterCountExcludingSpaces.length;
+    //console.log(caracteresEspeciales);
+    return caracteresEspeciales;
+    //const caracterEspeciales = characterCountExcludingSpaces.replace(text(/[^\\w]/gi, ''));
+    //luego hacer un array nuevo sin espacios y caracteres
+    //contar el largo de los abjetos con .length
+    
+  },
+
+  getAverageWordLength: (text) => {
+    //dividir la cadena en un array y eliminar los espacios y caracteres especiales
+    const averageWordLength = text.trim().split(/\s+/g); 
+    const palabrasFiltradas = averageWordLength.filter(function (elemento){
+      return isNaN(elemento); // Devuelve true si el elemento no es un número
+    });
+    const sumaPalabras = palabrasFiltradas.length;  //guardar la cantidad de palabras
+    //console.log("Cantidad de palabras " + sumaPalabras);
+    let contarCaracteres = 0;  //guardar la suma de caracteres encontrados
+
+    for (let i = 0; i< palabrasFiltradas.length; i++) { //iterar el array para saber cuantos caracteres hay
+      contarCaracteres = contarCaracteres + palabrasFiltradas[i].length;
+      //console.log("Caracteres "+ contarCaracteres);
+    }
+
+    const mediaPalabras = contarCaracteres/ sumaPalabras; //dividir la cantidad de caracteres y la cantidad de palabras
+    //console.log("media de palabras es "+ mediaPalabras)
+    return mediaPalabras;
+  },
+  
+  //return resultado; //obtener el resultado
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  getNumberCount: (text) => {
+    const numberCount = text.split(" "); 
+    let numCount = 0 //contador
+    for (let i = 0; i <numberCount.length; i++) { 
+      if (numberCount[i] !== '') { //si el no es numero
+        const number = Number(numberCount[i]) 
+        if (!isNaN(number)){ //si es un numero
+          numCount++  //se incrementa el contador
+        }
+      }
+    }
+    return numCount;
+    //const numContador = numberCount.filter(function (numero) {
+    // if(!isNaN === number){
+    //  return true
+
+    // console.log(arrFiltrado)
+    // if (!isNaN(parseInt(elemento))) {
+    //return elemento;
+
+    //let arrFiltrado = arrNumero.filter(function (value) {
+    //return value !== undefined; 
+  
+  },
+
+  getNumberSum: (text) => {
+    const arraySuma = text.trim().split(" "); //dividir la cadena filtrada en un array
+    const soloNumeros = arraySuma.filter(function (elemento){ 
+      return !isNaN(elemento)  //filtrar solo los numeros
+    });  
+    console.log(soloNumeros);
+    let contadorNumeros = 0; //acumulador
+    for (let i= 0; i < soloNumeros.length; i++){ //iterar el arreglo para pasar por cada elemento 
+      contadorNumeros = contadorNumeros + parseFloat(soloNumeros[i]); //sumar al acumulador el numero y sumarlo a la var
+    } 
+    console.log(contadorNumeros);
+    return contadorNumeros; //retornar el valor de la suma
+    /*let acumuladorNumeros = 0; //guardar los numeros en un acumulador
+     for (let i= 0; i< arraySuma.length; i++){ //iterar el array 
+      if (typeof parseInt(arraySuma[i]) !== isNaN(arraySuma[i])){//si es numero
+      console.log(arraySuma[i] + " es un numero? "+ !isNaN(arraySuma[i]));
+        acumuladorNumeros++ //si es un numero se incrementa el contador y se guarda
+      } if (typeof arraySuma[i] !== "string"){ //si no es número 
+        acumuladorNumeros += arraySuma[i] //sumar el contador con el numero siguiente del array
+        }
+    }
+    console.log (acumuladorNumeros);
+    return acumuladorNumeros;*/
+  }
+  //retornar el resultado*/
+
+   
+  /*for (let i = 0; i <numberSum.length; i++) { 
+      console.log(typeof parseInt(numberSum[i])) //tipo de dato que obtengo del array
+      if (typeof parseInt(numberSum[i]) === "number"){ //si el tipo de dato en el array es un numero
+         numSuma+= parseInt(numberSum[i])//sumar lo que hay en el acumulador y sumar el numero encontrado en el array
+
+      }
+    }
+      console.log("sumaDeNumeros",numSuma)
+      return numSuma;
+    }*/
+
+};
+export default analyzer
--- src/index.js
+++ src/index.js
@@ -13,37 +13,38 @@ const promedioLongitud= document.querySelector("li[data-testid= 'word-length-ave
 const textarea = document.querySelector("textarea");
 const limpiarBoton = document.getElementById("reset-button");
 limpiarBoton.addEventListener("click",function limpiarBoton (){
-    //limpiar contenido del textarea
-    textarea.value = "";
-    //limpiar los valores, comenzar 0
-    caracteres.innerHTML="Caracteres 0"
-    caracteresSinEspacios.innerHTML="caracteresSinEspacios 0"
-    palabras.innerHTML="palabras 0"
-    numeros.innerHTML="numeros 0"
-    sumaNumeros.innerHTML="sumaNumero 0"
-    promedioLongitud.innerHTML="promedioLongitud 0"
+  //limpiar contenido del textarea
+  textarea.value = "";
+  //limpiar los valores, comenzar 0
+  caracteres.innerHTML="Caracteres 0"
+  caracteresSinEspacios.innerHTML="caracteresSinEspacios 0"
+  palabras.innerHTML="palabras 0"
+  numeros.innerHTML="numeros 0"
+  sumaNumeros.innerHTML="sumaNumero 0"
+  promedioLongitud.innerHTML="promedioLongitud 0"
 });
 
 //Recuento de caracteres
 textarea.addEventListener("input",()=> {
-const charCount = analyzer.getCharacterCount(textarea.value);
-caracteres.innerHTML = charCount;
-//console.log(charCount);
+
+  const characterCount = analyzer.getCharacterCount(textarea.value);
+  caracteres.innerHTML="Caracteres " +  characterCount;
+
+  const palabrasCount = analyzer.getWordCount(textarea.value);
+  palabras.innerHTML="palabras " + palabrasCount;
+
+  const caractEspeciales = analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  caracteresSinEspacios.innerHTML = "caracteres Sin Espacios " + caractEspeciales; 
+
+  const numContar = analyzer.getNumberCount(textarea.value);
+  numeros.innerHTML = "numeros "+ numContar; 
+
+  const contadorNumeros = analyzer.getNumberSum(textarea.value);
+  sumaNumeros.innerHTML = "suma de numeros " + contadorNumeros;
+
+  const mediaPalabras = analyzer.getAverageWordLength(textarea.value);
+  promedioLongitud.innerHTML="promedio Longitud " + mediaPalabras;
+
 });
 
-//Recuento de palabras
-textarea.addEventListener("input",()=> {
-const wordCount = analyzer.getWordCount(textarea.value); 
-palabras.innerHTML = wordCount
-//contador de palabras, falta diferenciar los números de las palabras
-});
-
-//Recuento de caracteres sin espacio ni signos de puntuación
-
-
-
-//Recuento de números
-
-
-//Suma total de números
 //Calcular la longitud media de las palabras
